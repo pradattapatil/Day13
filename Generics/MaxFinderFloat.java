00fbd9f1e2bf367997e70b2df099f636284940ee
pradattapatil <pradattapatil@gmail.com>
2022-03-29 18:24:11 +0530

    Day13 Problem is added

---
+++ Generics/MaxFinderFloat.java
@@ -0,0 +1,14 @@
+package Day13.Generics;
+
+public class MaxFinderFloat {
+    public static Float findMax1(Float number1, Float number2, Float number3) {
+        Float max= number1;
+        if (max < number2){
+            max=number2;
+        }
+        if(max < number3){
+            max=number3;
+        }
+        return max;
+    }
+}
